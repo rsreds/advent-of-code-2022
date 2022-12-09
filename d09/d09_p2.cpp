ab9b46f7275ec7de921aadaaeb0fff47c4af5bd1
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2022-12-09 17:29:05 +0100

    Failed day 9 part 2

---
+++ d09/d09_p2.cpp
@@ -0,0 +1,65 @@
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <map>
+
+using Coordinates = std::pair<int, int>;
+
+int distance(const Coordinates a, const Coordinates b)
+{
+  return std::max(std::abs(a.first - b.first), std::abs(a.second - b.second));
+}
+
+int getInstruction(const char *filename)
+{
+  std::ifstream file(filename);
+  char direction{};
+  int move{};
+  std::array<std::map<Coordinates, bool>, 10> rope;
+  Coordinates lastHeadPosition{0, 0};
+  for (auto &knot : rope)
+  {
+    knot.insert({{0,0}, true});
+  }
+  while (file >> direction >> move)
+  {
+    for (int step = 1; step <= move; ++step)
+    {
+      switch (direction)
+      {
+      case 'U':
+        --lastHeadPosition.second;
+        break;
+      case 'L':
+        --lastHeadPosition.first;
+        break;
+      case 'R':
+        ++lastHeadPosition.first;
+        break;
+      case 'D':
+      default:
+        ++lastHeadPosition.second;
+        break;
+      }
+      rope[0].insert({lastHeadPosition, true});
+      for (int i = 1; i < 10;++i)
+      {
+        Coordinates lastPosition = (--rope[i-1].end())->first;
+        Coordinates newPosition = (--rope[i].end())->first;
+        if (distance(newPosition, lastPosition) > 1)
+        {
+          newPosition.first += (lastPosition.first - newPosition.first) != 0 ? (lastPosition.first - newPosition.first) / std::abs((lastPosition.first - newPosition.first)) : 0;
+          newPosition.second += (lastPosition.second - newPosition.second) != 0 ? (lastPosition.second - newPosition.second) / std::abs((lastPosition.second - newPosition.second)) : 0;
+        }
+        rope[i].insert({newPosition, true});
+      }
+    }
+  }
+  return rope[0].size();
+}
+
+int main(int argc, char const *argv[])
+{
+  std::cout << getInstruction(argv[1]) << '\n';
+  return 0;
+}
